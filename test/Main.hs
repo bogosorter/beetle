7f50fed30748b0ecf02d74e73b2ac3555d9738f6
bogosorter <luiswbarbosa@gmail.com>
2026-06-20 21:58:24 +0100

    added parser tests

---
+++ test/Main.hs
@@ -0,0 +1,48 @@
+module Main where
+
+import Parser (parseProgram)
+
+import Control.Monad (forM)
+import Control.Monad.Extra (concatMapM)
+import Data.List (isSuffixOf, sort)
+import System.Directory (doesDirectoryExist, listDirectory)
+import System.FilePath ((</>))
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, assertFailure)
+import Text.Megaparsec (errorBundlePretty)
+
+main :: IO ()
+main = do
+    let runner = defaultMain . testGroup "parse"
+
+    files <- getFiles "tests"
+    testResults <- mapM testParsing files
+    runner testResults
+
+testParsing :: FilePath -> IO TestTree
+testParsing path = do
+    content <- readFile path
+    return $ testCase path $ case parseProgram content of
+        Right _ -> return ()
+        Left error -> assertFailure (errorBundlePretty error)
+
+
+-- Utils
+
+getFiles :: FilePath -> IO [FilePath]
+getFiles path = do
+    allFiles <- walk path
+    return $ filter (".btl" `isSuffixOf`) allFiles
+
+walk :: FilePath -> IO [FilePath]
+walk path = do
+    isDirectory <- doesDirectoryExist path
+    case isDirectory of
+        False -> return [path]
+        True -> walkDirectory path
+
+walkDirectory :: FilePath -> IO [FilePath]
+walkDirectory path = do
+    children <- listDirectory path
+    let fullChildren = map (path </>) children
+    concatMapM walk fullChildren
